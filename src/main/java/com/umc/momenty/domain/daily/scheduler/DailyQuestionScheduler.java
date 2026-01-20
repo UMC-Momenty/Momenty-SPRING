af44b6f5a532919384cf9ddd8bdb0b5ffdeb07f0
0711kc <0711kc@naver.com>
2026-01-20 18:30:32 +0900

    ♻️ refactor: 일일 질문 생성 실패 시 재시도 기능 추가 (#3)

--- src/main/java/com/umc/momenty/domain/daily/scheduler/DailyQuestionScheduler.java
+++ src/main/java/com/umc/momenty/domain/daily/scheduler/DailyQuestionScheduler.java
@@ -1,13 +1,13 @@
 package com.umc.momenty.domain.daily.scheduler;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.hibernate.TransactionException;
+import org.springframework.core.retry.RetryException;
+import org.springframework.core.retry.RetryListener;
+import org.springframework.core.retry.RetryPolicy;
+import org.springframework.core.retry.RetryTemplate;
+import org.springframework.core.retry.Retryable;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.umc.momenty.domain.daily.entity.DailyQuestion;
-import com.umc.momenty.domain.daily.repository.DailyQuestionRepository;
-import com.umc.momenty.global.infra.GeminiProvider;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,28 +17,41 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 public class DailyQuestionScheduler {
 
-	@Value("${gemini.question.system-message}")
-	private String systemMessage;
+	private final DailyQuestionTask dailyQuestionTask;
 
-	@Value("${gemini.question.user-message}")
-	private String userMessage;
-
-	private final GeminiProvider geminiProvider;
-	private final DailyQuestionRepository dailyQuestionRepository;
-
-	// cron = "초 분 시 일 월 요일"
-	// "0 0 0 * * *" -> 매일 0시 0분 0초 (자정)에 실행
 	@Scheduled(cron = "0 0 0 * * *")
-	@Transactional
 	public void createDailyQuestion() {
 		log.info("Creating Daily Question...");
-		String question = geminiProvider.generateTextContent(systemMessage, userMessage);
 
-		DailyQuestion dailyQuestion = DailyQuestion.builder()
-			.question(question)
-			.build();
+		RetryPolicy retryPolicy = RetryPolicy.builder()
+			.maxRetries(3)
+			.includes(TransactionException.class).build();
+		RetryTemplate retryTemplate = new RetryTemplate(retryPolicy);
+		retryTemplate.setRetryListener(new TryCreatingDailyQuestionListener());
 
-		dailyQuestionRepository.save(dailyQuestion);
-		log.info("Daily Questions Saved Successfully [Question : {}]", question);
+		try {
+			retryTemplate.execute(new TryCreatingDailyQuestion());
+		} catch (RetryException e) {
+			log.warn("Failed to create Daily Question. Please add Daily Question manually.");
+		}
+	}
+
+	public static class TryCreatingDailyQuestionListener implements RetryListener {
+		@Override
+		public void beforeRetry(RetryPolicy retryPolicy, Retryable<?> retryable) {
+			log.warn("Retry Creating Daily Question...");
+		}
+
+		@Override
+		public void onRetryFailure(RetryPolicy retryPolicy, Retryable<?> retryable, Throwable throwable) {
+			log.warn("Failed to create Daily Question. [Message : {}]", throwable.getMessage());
+		}
+	}
+
+	private class TryCreatingDailyQuestion implements Retryable<String> {
+		@Override
+		public String execute() {
+			return dailyQuestionTask.createDailyQuestion();
+		}
 	}
 }
